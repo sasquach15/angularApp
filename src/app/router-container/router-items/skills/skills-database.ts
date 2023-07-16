70f436b7f35bd8c25bf5d4dd7d64760b5c177752
sasax <sasax15@autograf.pl>
2023-07-16 18:49:03 +0200

    skills component 100% completed with imgs and text

--- src/app/router-container/router-items/skills/skills-database.ts
+++ src/app/router-container/router-items/skills/skills-database.ts
@@ -111,6 +111,118 @@ export const skillData: any = {
             description: 'TELEPORT TO ENEMIES AND DESTROY THEM WITH POWERFUL KICK',
             selected: false,
         }
+    ],
+    SORCERESS: [
+        {
+            img: "../assets/photos/sorceress/skills/1.png",
+            title: 'FROZEN NOVA',
+            description: 'CREATES RING OF COLD ELEMENT SLOWING DOWN EVERYTHING IN AREA',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/2.png",
+            title: 'BLIZZARD',
+            description: 'UNLEASH MASSIVE SHARDS OF COLD TO DESTROY ENEMIES',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/3.png",
+            title: 'FROZEN ORB',
+            description: 'MAGICAL ORB THAT SPRAYS MULTIPLE ICE BOLTS',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/4.png",
+            title: 'LIGHTNING',
+            description: 'A LOT OF VOLTAGE STRAIGHT TO THE OPPONENT FACE',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/5.png",
+            title: 'TELEPORT',
+            description: 'INSTANTLY TELEPORTS YOU TO THE SELECTED SPOT',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/6.png",
+            title: 'THUNDER STORM',
+            description: 'DEADLY THUNDERSTORM THAT AIM AND STRIKE YOUR ENEMIES',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/7.png",
+            title: 'FIREBALL',
+            description: 'SHOOTS EXPLOSIVE SPHERE OF FIERY DEATH',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/8.png",
+            title: 'INFERNO',
+            description: 'CREATES PATH OF FIRE BEHIND YOU STEPS',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/sorceress/skills/9.png",
+            title: 'HYDRA',
+            description: 'SUMMONS MULTI-HEADED BEAST OF FLAME TO BURN ENEMIES INOT ASHES',
+            selected: false,
+        }
+    ],
+    NECROMANCER: [
+        {
+            img: "../assets/photos/necromancer/skills/1.png",
+            title: 'RAISE SKELETON',
+            description: 'SUMMONS UNDEAD SKELETON TO FIGHT BY YOUR SIDE',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/2.png",
+            title: 'FIRE GOLEM',
+            description: 'POWERFUL FIRE SERVANT STRAIGHT FROM HELL',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/3.png",
+            title: 'RESURRECT',
+            description: 'REVIVE KILLED ENEMY AS YOUR GUARDIAN',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/4.png",
+            title: 'CORPSE EXPLOSION',
+            description: 'ENEMY CRPSE EXPLODES DAMAGING NEARBY ENEMIES',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/5.png",
+            title: 'POISON NOVA',
+            description: 'EMITS EXPANDING RING OF CONDENSATED POISON',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/6.png",
+            title: 'POISON WEAPON',
+            description: 'POISONS YOUR WEAPON TO MAKE DEADLY ATTACKS',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/7.png",
+            title: 'BONE SPEAR',
+            description: 'SUMMONS A DEADLY SPIKE OF BONE TO IMPALE ENEMIES',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/8.png",
+            title: 'BONE PRISON',
+            description: 'CREATES BONE CAGE AROUND YOUR OPPONENT',
+            selected: false,
+        },
+        {
+            img: "../assets/photos/necromancer/skills/9.png",
+            title: 'BONE SPIRIT',
+            description: 'SUMMONS BONE SPIRIT TO TRACK YOUR ENEMIES UNTIL THEY DIE',
+            selected: false,
+        }
     ]
 
 
